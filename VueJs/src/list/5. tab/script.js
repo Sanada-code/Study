c9fe4847c0407a174b287f89150d2ff0752c1ef1
Sanada <sanehikokumagai@gmail.com>
2021-07-07 02:04:33 +0900

    tabサンプル

--- VueJs/src/list/5. tab/script.js
+++ VueJs/src/list/5. tab/script.js
@@ -1,37 +1,11 @@
 new Vue({
-    el: '#app',
-    data: {
-      lists:[
-        {
-          name:'item1',
-          checked:false,
-        },
-        {
-          name:'item2',
-          checked:true,
-        },
-        {
-          name:'item3',
-          checked:false,
-        },
-      ],
-      newItem:'',
+  el: "#app",
+  data: {
+    isActive: "1",
+  },
+  methods: {
+    change: function (num) {
+      this.isActive = num;
     },
-    
-    methods: {
-      AddItem: function(event) {
-        if (this.newItem == '') {
-            return
-        }
-        let item = {
-          name:this.newItem,
-          checked: false,
-        }
-        this.lists.push(item)
-        this.newItem = ''
-      },
-      DelItem: function(index) {
-        this.lists.splice(index, 1);
-      },
-    },
-  });
+  },
+});
